cd750206ec39836d258b5312b3c6ea3af490fe1f
krave1986 <krave@qq.com>
2020-03-10 03:21:08 +0800

    准备测试

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div>
+		<MessageList :messages="messages" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MessageList from "./components/MessageList";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	data: () => ({
+		messages: ["Hey John", "Howdy Paco"]
+	}),
+	components: { MessageList }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	margin: 0;
 }
 </style>
