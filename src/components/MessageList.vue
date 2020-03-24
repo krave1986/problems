823954425baf1f96c98e930750ab7af2db1e92c9
krave1986 <krave@qq.com>
2020-03-25 02:10:20 +0800

    准备工作

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,14 +1,16 @@
 <template>
 	<ul>
-		<li v-for="(message, index) in messages" :key="index">
-			{{ message }}
-		</li>
+		<Message :key="index" :message="message" v-for="(message, index) in messages" />
 	</ul>
 </template>
 
 <script>
+import Message from "./Message";
 export default {
-	name: "list",
-	props: ["messages"]
+	name: "MessageList",
+	props: ["messages"],
+	components: {
+		Message
+	}
 };
 </script>
